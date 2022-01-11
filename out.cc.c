0718440c5790de95e92bc82adef7d1c7b6668d43
耿哲 <gamejoye@gmail.com>
2022-01-11 17:54:09 +0800

    ？？？

--- out.cc.c
+++ out.cc.c
@@ -3,6 +3,13 @@ void main()
 {
     printf("YES\n");
     printf("NO\n");
-    printf("asdas\n");
-    printf("但却无法\n");
+    printf("YES\n");
+    printf("NO\n");
+    printf("YES\n");
+    printf("NO\n");
+    printf("YES\n");
+    printf("NO\n");
+    printf("YES\n");
+    printf("NO\n");
+    printf("???????");
 }
