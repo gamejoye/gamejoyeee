6198c68e6a2509dc60935a90275246d1a37aaea9
耿哲 <gamejoye@gmail.com>
2022-01-11 17:51:03 +0800

    试试revert

--- out.cc.c
+++ out.cc.c
@@ -3,12 +3,6 @@ void main()
 {
     printf("YES\n");
     printf("NO\n");
-    printf("YES\n");
-    printf("NO\n");
-    printf("YES\n");
-    printf("NO\n");
-    printf("YES\n");
-    printf("NO\n");
-    printf("YES\n");
-    printf("NO\n");
+    printf("asdas\n");
+    printf("但却无法\n");
 }
